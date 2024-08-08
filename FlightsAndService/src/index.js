02a7c1e75b5cf6d669a800f5a26ea9703830984f
Amit Singh <amitkarun4559@gmail.com>
2024-08-08 18:09:13 +0530

    Added airport seeders

--- FlightsAndService/src/index.js
+++ FlightsAndService/src/index.js
@@ -3,14 +3,19 @@ const bodyParser = require("body-parser");
 const { PORT } = require('./config/serverConfig');
 const ApiRoutes = require("./routes/index");
 
+const db = require("./models/index");
 const setupAndStartServer = async () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
     app.use("/api", ApiRoutes);
 
-    app.listen(PORT, ()=>{
+    app.listen(PORT, async ()=>{
         console.log(`Server is started at ${PORT}`);
+        if(process.env.SYNC_DB){
+            db.sequelize.sync({alter:true})
+        }
+        
     });
 }
 
